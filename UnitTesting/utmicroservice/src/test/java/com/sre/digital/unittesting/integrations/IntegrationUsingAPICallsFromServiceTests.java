df88f0c7c7b8f05ae4a739d26eb5ef7e0c34c559
singaraju-sreedhar <singaraju.sreedhar@101digital.io>
2023-04-27 15:50:12 +0530

    folder name changed
    folder changed

--- UnitTesting/utmicroservice/src/test/java/com/sre/digital/unittesting/integrations/IntegrationUsingAPICallsFromServiceTests.java
+++ UnitTesting/utmicroservice/src/test/java/com/sre/digital/unittesting/integrations/IntegrationUsingAPICallsFromServiceTests.java
@@ -2,14 +2,18 @@ package com.sre.digital.unittesting.integrations;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.sre.digital.unittesting.repository.Itemrepository;
 import com.sre.digital.unittesting.service.BusinessService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(classes = {BusinessService.class})
+@AutoConfigureMockMvc
 public class IntegrationUsingAPICallsFromServiceTests {
 
     @MockBean
@@ -17,6 +21,13 @@ public class IntegrationUsingAPICallsFromServiceTests {
 
     @Autowired
     BusinessService businessService;
+
+    @MockBean
+    RedisTemplate redisTemplate;
+
+    @MockBean
+    Itemrepository itemrepository;
+
 
     @Test
     public  void serviceRESTIntegrationTest()
